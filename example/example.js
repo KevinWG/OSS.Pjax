57cd4d4fdefbf0051de3a682a57c36a68ce0305c
kevin <1985088337@qq.com>
2021-12-08 17:27:52 +0800

    更新示例

--- example/example.js
+++ example/example.js
@@ -1,35 +1,19 @@
  var OssPjax = {
      instance: null,
-    methods:
+    start: function()
     {
-        remoteError: function(eMsg, textState, xhr) {
-            if (textState === 'error') {
-               alert('当前请求出错，请检查网络或稍后再试!');         
-            }
-       },
-     
-        complete: function(newState) {
-             console.info("加载完成！");
-        }
-    },
-    start: function(isDev)
-    {
-        var ossPjax = this;
          // 初始化实例
-         ossPjax.instance = $(document).osspjax({
-             wraper: "#oss-page-wraper",
-             nameSpc: "oss-pjax",
-             methods: ossPjax.methods
+         this.instance = $(document).osspjax({
+             wraper: "#oss-page-wraper"
          });
 
          // 定义全局goTo方法
-         window.goTo = function(url, title) {
-            // ossPjax.instance.osspjax("goTo", { url: url, title: title });
-            $(document).osspjax("goTo", { url: url, title: title });
+         window.goTo = function(url) {   
+            $(document).osspjax("goTo",  url);
          };
     }
  };
 
  $(function(){
-    OssPjax.start(1);
+    OssPjax.start();
  })
