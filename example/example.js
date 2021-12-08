17e3e8cccafd96a4f375655859eef043970ee024
kevin <1985088337@qq.com>
2021-12-08 18:16:30 +0800

    简化用例

--- example/example.js
+++ example/example.js
@@ -1,21 +1,5 @@
- var OssPjax = {
-    instance: null,
-    goTo:null,
-    
-    start: function () {
-        const self = this;
-        // 初始化实例
-        self.instance = $(document).osspjax({
-            wraper: "#oss-page-wraper"
-        })
-
-        // 除了a标签，也可以直接使用 goTo 方法
-        self.goTo = function (url) {
-            self.instance.osspjax("goTo", url);
-        };
-    }
- };
-
  $(function(){
-    OssPjax.start();
- })
+    $(document).osspjax({
+        wraper: "#oss-page-wraper"
+    })
+ });
